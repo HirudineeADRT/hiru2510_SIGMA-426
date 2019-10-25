90ae57ef342cfdf568b354d61946af5d296bdd93
HirudineeADRT <hirudinee@adroitlogic.com>
2019-10-25 11:26:28 +0530

    SIGMA-426 test

---
+++ hh/lambda.js
@@ -0,0 +1,19 @@
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+
+    cognito_idp.listUsers({
+        UserPoolId: process.env.UserPoolId_cognitotesthirudinee,
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            // implement error handling logic here
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+
+    callback(null, { "message": "Successfully executed" });
+}
