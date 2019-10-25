494a11712b64d944b0a59660e0bc92876fe38cc2
HirudineeADRT <hirudinee@adroitlogic.com>
2019-10-25 13:51:00 +0530

    jj

--- hh/lambda.js
+++ hh/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = function (event, context, callback) {
@@ -8,11 +9,22 @@ exports.handler = function (event, context, callback) {
         Limit: 10
     }, function (error, data) {
         if (error) {
-            // implement error handling logic here
+            // implement error handling logic here kkk
             throw error;
         }
         // your logic goes within this block
     });
+    ddb.query({
+        TableName: 'yyy'
+        , ExpressionAttributeValues: { ':jj': 'kk ' }, KeyConditionExpression: 'jjj < :jj', FilterExpression: 'nn n nmn = :jj'
+    }).promise()
+        .then((data) => {
+            //your logic goes here
+        })
+        .catch((err) => {
+            //handle error
+        });
+
 
 
     callback(null, { "message": "Successfully executed" });
